769150e7e8829d9d1cc1469f020e5fbba4905f6c
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-15 20:19:43 +0200

    fix UI problems
    - fixed Wellcome menu auth login UI size
    - Renew Logo

--- AwesomeFood/AwesomeFood/Product/Button/FacebookButton.swift
+++ AwesomeFood/AwesomeFood/Product/Button/FacebookButton.swift
@@ -15,24 +15,29 @@ struct FacebookButton: View {
             Button {
                 onTap()
             } label: {
-                HStack {
-                    Image(IconItems.Social.facebook .rawValue)
-                        .resizable()
-                        .background(Color.white
-                            .cornerRadius(20)
-                            .frame(width: 40,height: 40)
-                        )
-                        .frame(width: 30, height: 30)
-                        .padding(.trailing)
-                    Text(LocaleKeys.Auth.facebook.rawValue.locale())
-                    
-                }.tint(.white)
-                    .font(.title2)
-                    .padding(.all,PagePadding.All.normal.rawValue)
-                
-            } .background(Color.deepSkyBlue)
-                .controlSize(.large)
-            .cornerRadius(RadiusItems.radius)
+                ZStack {
+                    RoundedRectangle(cornerRadius: RadiusItems.radius)
+                        .foregroundColor(Color.deepSkyBlue)
+                        .frame(width: 360, height: 66)
+                    HStack {
+                        Image(IconItems.Social.facebook .rawValue)
+                            .resizable()
+                            .background(Color.white
+                                .cornerRadius(20)
+                                .frame(width: 40,height: 40)
+                            )
+                            .frame(width: 30, height: 30)
+                            .padding(.trailing)
+                        
+                        Text(LocaleKeys.Auth.facebook.rawValue.locale())
+                        
+                    }.tint(.white)
+                        .font(.title2)
+                        .padding(.all,PagePadding.All.normal.rawValue)
+                        
+                }
+
+            }
         }
     }
 }
