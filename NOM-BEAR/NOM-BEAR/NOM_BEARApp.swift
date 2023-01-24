aa6eb157e8299155f709e87c64a8b3286fc15135
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-24 16:46:21 +0200

    NOM-BEAR Started to be developed
    Description of the application :
    If you want, you can go with a vehicle that shows the nearest beer purchasing point and get your beer and return.

---
+++ NOM-BEAR/NOM-BEAR/NOM_BEARApp.swift
@@ -0,0 +1,17 @@
+//
+//  NOM_BEARApp.swift
+//  NOM-BEAR
+//
+//  Created by NomoteteS on 24.01.2023.
+//
+
+import SwiftUI
+
+@main
+struct NOM_BEARApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
