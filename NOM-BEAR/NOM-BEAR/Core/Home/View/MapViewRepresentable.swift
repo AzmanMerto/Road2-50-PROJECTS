1da433d77837dd16545c51373f428205c7e221aa
NomoteteS <nomotetes.onetrue@icloud.com>
2023-01-27 06:18:16 +0200

    NOM-BEAR locations coordinates property
    -feat getting locations coordinates
    -feat annotation on map
    -feat destinations on map

--- NOM-BEAR/NOM-BEAR/Core/Home/View/MapViewRepresentable.swift
+++ NOM-BEAR/NOM-BEAR/Core/Home/View/MapViewRepresentable.swift
@@ -22,10 +22,12 @@ struct MapViewRepresentable : UIViewRepresentable {
         
         return mapView
     }
+
     
     func updateUIView(_ uiView: UIViewType, context: Context) {
-        if let selectedLocation = locationViewModel.selectedLocation {
-            
+        if let coordinate = locationViewModel.selectedLocationCoordinate {
+            context.coordinator.addAndSelectAnnotation(withCoordinate: coordinate)
+            context.coordinator.configurePolyline(withDestinationCoordinate: coordinate)
         }
     }
 
@@ -36,14 +38,20 @@ struct MapViewRepresentable : UIViewRepresentable {
 
 extension MapViewRepresentable {
     class MapCoordinator: NSObject, MKMapViewDelegate {
-        let parent : MapViewRepresentable
         
+        // MARK: - Properties
+        let parent : MapViewRepresentable
+        var userLocationCoordinate: CLLocationCoordinate2D?
+        
+        // MARK: - Lifecycle
         init(parent: MapViewRepresentable) {
             self.parent = parent
             super.init()
         }
         
+        // MARK: - MKMapViewDelegate
         func mapView(_ mapView: MKMapView, didUpdate userLocation: MKUserLocation) {
+            self.userLocationCoordinate = userLocation.coordinate
             let region = MKCoordinateRegion(
                 center: CLLocationCoordinate2D(
                     latitude: userLocation.coordinate.latitude,
@@ -52,6 +60,53 @@ extension MapViewRepresentable {
                 span: MKCoordinateSpan(latitudeDelta: 0.05, longitudeDelta: 0.05)
             )
             parent.mapView.setRegion(region, animated: true)
+        }
+        
+        func mapView(_ mapView: MKMapView, rendererFor overlay: MKOverlay) -> MKOverlayRenderer {
+            let polyline = MKPolylineRenderer(overlay: overlay)
+            polyline.strokeColor = .systemBrown
+            polyline.lineWidth = 6
+            return polyline
+        }
+        
+        // MARK: - Helpers
+
+        func addAndSelectAnnotation(withCoordinate coordinate: CLLocationCoordinate2D) {
+            parent.mapView.removeAnnotations(parent.mapView.annotations)
+            let anno = MKPointAnnotation()
+            anno.coordinate = coordinate
+            self.parent.mapView.addAnnotation(anno)
+            self.parent.mapView.selectAnnotation(anno, animated: true)
+            
+            parent.mapView.showAnnotations(parent.mapView.annotations, animated: true)
+        }
+        
+        func configurePolyline(withDestinationCoordinate coordinate: CLLocationCoordinate2D) {
+            guard let userLocationCoordinate = self.userLocationCoordinate else { return }
+            getDestinationRoute(from: userLocationCoordinate,
+                                to: coordinate) { route in
+                self.parent.mapView.addOverlay(route.polyline)
+    
+            }
+        }
+        
+        func getDestinationRoute(from userLocation: CLLocationCoordinate2D,
+                                 to destination: CLLocationCoordinate2D,
+                                 completion: @escaping(MKRoute) -> Void) {
+            let userPlacemark = MKPlacemark(coordinate: userLocation)
+            let destPlacemark = MKPlacemark(coordinate: destination)
+            let request = MKDirections.Request()
+            request.source = MKMapItem(placemark: userPlacemark)
+            request.destination = MKMapItem(placemark: destPlacemark)
+            let directions = MKDirections(request: request)
+            
+            directions.calculate { response, error in
+                if let error = error { print(error.localizedDescription); return}
+                
+                guard let route = response?.routes.first else { return }
+                
+                completion(route)
+            }
         }
     }
 }
