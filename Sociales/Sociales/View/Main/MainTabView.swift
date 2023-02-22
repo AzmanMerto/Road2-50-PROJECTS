b3b3c65f2cb542a340cf0781f3ab55c27064b72f
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-22 14:16:39 +0200

    Sociales feat: Tabbed Interface

---
+++ Sociales/Sociales/View/Main/MainTabView.swift
@@ -0,0 +1,45 @@
+//
+//  MainTabView.swift
+//  Sociales
+//
+//  Created by NomoteteS on 22.02.2023.
+//
+
+import SwiftUI
+
+struct MainTabView: View {
+    var body: some View {
+        TabView {
+            FeedView()
+                .tabItem {
+                    Image(systemName: "house")
+                }
+            
+            SearchView()
+                .tabItem {
+                    Image(systemName: "magnifyingglass")
+                }
+            UploadPostView()
+                .tabItem {
+                    Image(systemName: "plus.square")
+                }
+            
+            NotificationView()
+                .tabItem {
+                    Image(systemName: "heart")
+                }
+            
+            ProfileView()
+                .tabItem {
+                    Image(systemName: "person")
+                }
+        }
+        .tint(.black)
+    }
+}
+
+struct MainTabView_Previews: PreviewProvider {
+    static var previews: some View {
+        MainTabView()
+    }
+}
---
+++ Sociales/Sociales/View/Notifications/NotificationView.swift
@@ -0,0 +1,20 @@
+//
+//  NotificationView.swift
+//  Sociales
+//
+//  Created by NomoteteS on 22.02.2023.
+//
+
+import SwiftUI
+
+struct NotificationView: View {
+    var body: some View {
+        Text("Notifications")
+    }
+}
+
+struct NotificationView_Previews: PreviewProvider {
+    static var previews: some View {
+        NotificationView()
+    }
+}
