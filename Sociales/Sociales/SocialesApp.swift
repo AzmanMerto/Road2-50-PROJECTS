7f5011ad55d520e2257712496f92d74eefe6c202
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-22 13:15:57 +0200

    Sociales Started to be developed

---
+++ Sociales/Sociales/SocialesApp.swift
@@ -0,0 +1,17 @@
+//
+//  SocialesApp.swift
+//  Sociales
+//
+//  Created by NomoteteS on 22.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct SocialesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
