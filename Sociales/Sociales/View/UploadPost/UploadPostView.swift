2bedf57998f4592129b75ab196d332536b229df0
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-23 03:07:17 +0200

    Sociales feat: UploadImageUI

--- Sociales/Sociales/View/UploadPost/UploadPostView.swift
+++ Sociales/Sociales/View/UploadPost/UploadPostView.swift
@@ -8,8 +8,53 @@
 import SwiftUI
 
 struct UploadPostView: View {
+    
+    @State private var selectedImage: UIImage?
+    @State var postImage: Image?
+    @State var captionText = ""
+    
     var body: some View {
-        Text("Upload Post")
+        VStack {
+            if postImage == nil {
+                Button {
+                    // Will be action
+                } label: {
+                    Image(systemName: "plus")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 180,
+                               height: 180)
+                        .clipped()
+                        .padding(.top,56)
+                        .foregroundColor(.red)
+                }
+            } else {
+                HStack(alignment: .top) {
+                    Image("testPostImage")
+                        .resizable()
+                        .scaledToFill()
+                        .frame(width: 96,
+                               height: 96)
+                    
+                    TextField("Enter your caption", text: $captionText)
+                }
+                .padding()
+                
+                Button {
+                    // Will be action
+                } label: {
+                    Text("Share")
+                        .font(.system(.caption,
+                                      weight: .semibold))
+                        .frame(width: 360,
+                               height: 50)
+                        .cornerRadius(5)
+                        .foregroundColor(.white)
+                }
+                .padding()
+            }
+            Spacer()
+        }
     }
 }
 
