723fcecebac779abad629a0b7cc65d9f6e93000e
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-23 04:59:22 +0200

    Sociales feat: Add LoginView Component's

--- Sociales/Sociales/View/Auth/LoginView.swift
+++ Sociales/Sociales/View/Auth/LoginView.swift
@@ -8,6 +8,10 @@
 import SwiftUI
 
 struct LoginView: View {
+    
+    @State private var email = ""
+    @State private var password = ""
+    
     var body: some View {
         ZStack {
             LinearGradient(gradient: Gradient(colors: [Color.blue, Color.pink]),
@@ -15,14 +19,73 @@ struct LoginView: View {
                                        endPoint: .bottom)
             .edgesIgnoringSafeArea(.all)
             VStack {
+                Spacer()
                 Text("Sociales")
-                    .font(.largeTitle)
+                    .font(.system(.largeTitle, weight: .black))
                     .foregroundColor(.white)
+                    .padding()
+                //  MARK: - LoginView email & password fields
+                VStack(spacing: 20) {
+                    // email field
+                    CustomTextField(text: $email,
+                                    placeholder: Text("Email"),
+                                    imageName: "envelope")
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal,32)
+                    // password field
+                    CustomSecureField(text: $password,
+                                      placeholder: Text("Password"))
+                        .padding()
+                        .background(Color(.init(white: 1, alpha: 0.15)))
+                        .cornerRadius(10)
+                        .foregroundColor(.white)
+                        .padding(.horizontal,32)
+                }
+                // MARK: - LoginView forgot password
+                HStack {
+                    Spacer()
+                    Button {
+                        
+                    } label: {
+                        Text("Forgot Password?")
+                            .font(.system(.caption))
+                            .foregroundColor(.white)
+                    }
+                    .padding(.init(top: 10, leading: 0, bottom: 0, trailing: 40))
+                }
+                // MARK: - LoginView sign in
+                Button {
+                    
+                } label: {
+                    Text("Sign In")
+                        .font(.headline)
+                        .foregroundColor(.white)
+                        .frame(width: 360, height: 50)
+                        .background(Color(UIColor.separator))
+                        .clipShape(Capsule())
+                        .padding()
+                }
                 
                 Spacer()
+                
+                // MARK: - LoginView go to sign up
+                Button {
+                    
+                } label: {
+                    HStack {
+                        Text("Dont have an account?")
+                            .font(.system(.caption2))
+                        
+                        Text("Sign Up")
+                            .font(.system(.caption2,weight: .semibold))
+                    }
+                    .tint(.white)
+                }
             }
         }
-        
     }
 }
 
