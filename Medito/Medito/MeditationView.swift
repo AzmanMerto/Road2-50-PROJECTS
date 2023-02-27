06fafd8b118af0859764c4df498dad9da5c692c4
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-27 04:22:28 +0200

    Medito // Add MeditationView

---
+++ Medito/Medito/MeditationView.swift
@@ -0,0 +1,65 @@
+//
+//  MeditationView.swift
+//  Medito
+//
+//  Created by NomoteteS on 27.02.2023.
+//
+
+import SwiftUI
+
+struct MeditationView: View {
+    var body: some View {
+        VStack(spacing: 0) {
+            Image("testPhoto")
+                .resizable()
+                .scaledToFill()
+                .frame(height: UIScreen.main.bounds.height * 2 / 3)
+            
+            // MARK: Meditation Details
+            ZStack {
+                Color(red: 24/255, green: 23/255, blue: 22/255)
+                
+                VStack(alignment: .leading, spacing: 24) {
+                    VStack(alignment: .leading, spacing: 8) {
+                        Text("Music")
+                        
+                        Text("0s")
+                    }
+                    .font(.subheadline)
+                    .textCase(.uppercase)
+                    .opacity(0.7)
+                    
+                    Text("1 Minute Relaxing Meditation")
+                        .font(.title)
+                    
+                    Button {
+                        
+                    } label: {
+                        Label("Play", systemImage: "play.fill")
+                            .font(.headline)
+                            .foregroundColor(.black)
+                            .padding(.vertical,10)
+                            .frame(maxWidth: .infinity)
+                            .background(.white)
+                            .cornerRadius(20 )
+                    }
+
+                    
+                    Text("Clear your mind and slumber into nothingness. Allocate only a few moments for a quick breathe")
+                    
+                    Spacer()
+                }
+                .foregroundColor(.white)
+                .padding(20)
+            }
+            .frame(height: UIScreen.main.bounds.height * 2 / 3)
+        }
+        .ignoresSafeArea()
+    }
+}
+
+struct MeditationView_Previews: PreviewProvider {
+    static var previews: some View {
+        MeditationView()
+    }
+}
