b6828bc29dc5bf41cb0b5e728e72bf4ecd1ed132
NomoteteS <nomotetes.onetrue@icloud.com>
2023-02-27 04:11:00 +0200

    Medito Started to be developed

---
+++ Medito/Medito/MeditoApp.swift
@@ -0,0 +1,17 @@
+//
+//  MeditoApp.swift
+//  Medito
+//
+//  Created by NomoteteS on 27.02.2023.
+//
+
+import SwiftUI
+
+@main
+struct MeditoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
